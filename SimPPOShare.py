b126096182f222d57e0f2b51fb655c9a262f81d7
Simon Blöthner <Simon@MacBook-Pro-168.fritz.box>
2024-06-29 20:40:36 +0200

    Added market share

---
+++ SimPPOShare.py
@@ -0,0 +1,398 @@
+import torch.nn as nn
+from torch.distributions import MultivariateNormal
+from torch.distributions import Categorical
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import os
+
+import torch
+import numpy as np
+
+
+np.random.seed(1)
+torch.manual_seed(1)
+torch.cuda.manual_seed(1)
+
+path_ = os.getcwd()
+local = path_.find('Simon') > 0
+if local:
+    path_ = '/Users/Simon/Documents/Projects/EWF/Research/PhD/ABMs/collusion'
+else:
+    path_ = 'collusion'
+
+################################## set device ##################################
+print("============================================================================================")
+# set device to cpu or cuda
+device = torch.device('cpu')
+if (torch.cuda.is_available()):
+    device = torch.device('cuda:0')
+    torch.cuda.empty_cache()
+    print("Device set to : " + str(torch.cuda.get_device_name(device)))
+else:
+    print("Device set to : cpu")
+print("============================================================================================")
+
+
+################################## PPO Policy ##################################
+class RolloutBuffer:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.state_values = []
+        self.is_terminals = []
+
+    def clear(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+        del self.state_values[:]
+        del self.is_terminals[:]
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, has_continuous_action_space, action_std_init):
+        super(ActorCritic, self).__init__()
+
+        self.has_continuous_action_space = has_continuous_action_space
+
+        if has_continuous_action_space:
+            self.action_dim = action_dim
+            self.action_var = torch.full((action_dim,), action_std_init * action_std_init).to(device)
+        # actor
+        if has_continuous_action_space:
+            self.actor = nn.Sequential(nn.Linear(state_dim, 64), nn.Tanh(), nn.Linear(64, 64), nn.Tanh(),
+                nn.Linear(64, action_dim), nn.Tanh())
+        else:
+            self.actor = nn.Sequential(nn.Linear(state_dim, 64), nn.Tanh(), nn.Linear(64, 64), nn.Tanh(),
+                nn.Linear(64, action_dim), nn.Softmax(dim=-1))
+        # critic
+        self.critic = nn.Sequential(nn.Linear(state_dim, 64), nn.Tanh(), nn.Linear(64, 64), nn.Tanh(), nn.Linear(64, 1))
+
+    def set_action_std(self, new_action_std):
+        if self.has_continuous_action_space:
+            self.action_var = torch.full((self.action_dim,), new_action_std * new_action_std).to(device)
+        else:
+            print("--------------------------------------------------------------------------------------------")
+            print("WARNING : Calling ActorCritic::set_action_std() on discrete action space policy")
+            print("--------------------------------------------------------------------------------------------")
+
+    def forward(self):
+        raise NotImplementedError
+
+    def act(self, state):
+        if self.has_continuous_action_space:
+            action_mean = self.actor(state)
+            cov_mat = torch.diag(self.action_var).unsqueeze(dim=0)
+            dist = MultivariateNormal(action_mean, cov_mat)
+        else:
+            action_probs = self.actor(state)
+            dist = Categorical(action_probs)
+
+        action = dist.sample()
+        # action = torch.sigmoid(action)
+        action = (torch.tanh(action) + 1) / 2
+        action_logprob = dist.log_prob(action)
+        state_val = self.critic(state)
+
+        return action.detach(), action_logprob.detach(), state_val.detach()
+
+    def evaluate(self, state, action):
+        if self.has_continuous_action_space:
+            if model.period == 302037:
+                print('!!')
+            action_mean = self.actor(state)
+
+            action_var = self.action_var.expand_as(action_mean)
+            cov_mat = torch.diag_embed(action_var).to(device)
+            if torch.isnan(action_mean).any():
+                print('!')
+            dist = MultivariateNormal(action_mean, cov_mat)
+
+            # For Single Action Environments.
+            if self.action_dim == 1:
+                action = action.reshape(-1, self.action_dim)
+        else:
+            action_probs = self.actor(state)
+            dist = Categorical(action_probs)
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+        state_values = self.critic(state)
+
+        return action_logprobs, state_values, dist_entropy
+
+
+class Firm:
+    def __init__(self, number, state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip,
+                 has_continuous_action_space, action_std_init=0.6):
+        self.has_continuous_action_space = has_continuous_action_space
+        self.id = number
+
+        if has_continuous_action_space:
+            self.action_std = action_std_init
+
+        self.gamma = gamma
+        self.eps_clip = eps_clip
+        self.K_epochs = K_epochs
+
+        self.buffer = RolloutBuffer()
+
+        self.policy = ActorCritic(state_dim, action_dim, has_continuous_action_space, action_std_init).to(device)
+        self.optimizer = torch.optim.Adam([{'params': self.policy.actor.parameters(), 'lr': lr_actor},
+            {'params': self.policy.critic.parameters(), 'lr': lr_critic}])
+
+        self.policy_old = ActorCritic(state_dim, action_dim, has_continuous_action_space, action_std_init).to(device)
+        self.policy_old.load_state_dict(self.policy.state_dict())
+
+        self.MseLoss = nn.MSELoss()
+
+    def set_action_std(self, new_action_std):
+        if self.has_continuous_action_space:
+            self.action_std = new_action_std
+            self.policy.set_action_std(new_action_std)
+            self.policy_old.set_action_std(new_action_std)
+        else:
+            print("--------------------------------------------------------------------------------------------")
+            print("WARNING : Calling PPO::set_action_std() on discrete action space policy")
+            print("--------------------------------------------------------------------------------------------")
+
+    def decay_action_std(self, action_std_decay_rate, min_action_std):
+        print("--------------------------------------------------------------------------------------------")
+        if self.has_continuous_action_space:
+            self.action_std = self.action_std - action_std_decay_rate
+            self.action_std = round(self.action_std, 4)
+
+            if (self.action_std <= min_action_std):
+                self.action_std = min_action_std
+                print("setting actor output action_std to min_action_std : ", self.action_std)
+            else:
+                print("setting actor output action_std to : ", self.action_std)
+            self.set_action_std(self.action_std)
+
+        else:
+            print("WARNING : Calling PPO::decay_action_std() on discrete action space policy")
+        print("--------------------------------------------------------------------------------------------")
+
+    def select_action(self, state):
+        if self.has_continuous_action_space:
+            with torch.no_grad():
+                state = torch.FloatTensor(state).to(device)
+                action, action_logprob, state_val = self.policy_old.act(state)
+
+            self.buffer.states.append(state)
+            self.buffer.actions.append(action)
+            self.buffer.logprobs.append(action_logprob)
+            self.buffer.state_values.append(state_val)
+
+            return action.detach().cpu().numpy().flatten()
+        else:
+            with torch.no_grad():
+                state = torch.FloatTensor(state).to(device)
+                action, action_logprob, state_val = self.policy_old.act(state)
+
+            self.buffer.states.append(state)
+            self.buffer.actions.append(action)
+            self.buffer.logprobs.append(action_logprob)
+            self.buffer.state_values.append(state_val)
+
+            return action.item()
+
+    def update(self):
+        # Monte Carlo estimate of returns
+        rewards = []
+        discounted_reward = 0
+        for reward, is_terminal in zip(reversed(self.buffer.rewards), reversed(self.buffer.is_terminals)):
+            if is_terminal:
+                discounted_reward = 0
+            discounted_reward = reward + (self.gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+
+        # Normalizing the rewards
+        rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-7)
+
+        # convert list to tensor
+        old_states = torch.squeeze(torch.stack(self.buffer.states, dim=0)).detach().to(device)
+        old_actions = torch.squeeze(torch.stack(self.buffer.actions, dim=0)).detach().to(device)
+        old_logprobs = torch.squeeze(torch.stack(self.buffer.logprobs, dim=0)).detach().to(device)
+        old_state_values = torch.squeeze(torch.stack(self.buffer.state_values, dim=0)).detach().to(device)
+
+        # calculate advantages
+        advantages = rewards.detach() - old_state_values.detach()
+        # Optimize policy for K epochs
+        for k in range(self.K_epochs):
+            if self.id == 1 and model.period == 302037 and k == 20:
+                print('!!')
+            # Evaluating old actions and values
+            logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+
+            # match state_values tensor dimensions with rewards tensor
+            state_values = torch.squeeze(state_values)
+
+            # Finding the ratio (pi_theta / pi_theta__old)
+            ratios = torch.exp(logprobs - old_logprobs.detach())
+
+            # Finding Surrogate Loss
+            surr1 = ratios * advantages
+            surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
+
+            # final loss of clipped objective PPO
+            loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
+
+            # take gradient step
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
+
+        # Copy new weights into old policy
+        self.policy_old.load_state_dict(self.policy.state_dict())
+
+        # clear buffer
+        self.buffer.clear()
+
+    def save(self, checkpoint_path):
+        torch.save(self.policy_old.state_dict(), checkpoint_path)
+
+    def load(self, checkpoint_path):
+        self.policy_old.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
+        self.policy.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
+
+
+class CollusionModel():
+    def __init__(self, n, iterations, shares):
+        self.num_agents = n
+        self.period = 0
+        self.schedule = [Firm(firm, state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, has_continuous_action_space,
+                action_std) for firm in range(self.num_agents)]
+        self.prices = np.zeros((iterations, n))
+        self.demand = np.zeros((iterations, n))
+        self.profits = np.zeros((iterations, n))
+        self.shares = shares
+
+    def initialize(self, iterations, n):
+        self.prices = np.zeros((iterations, n))
+        self.demand = np.zeros((iterations, n))
+        self.profits = np.zeros((iterations, n))
+        self.period = 0
+
+    def step(self):
+
+        action = []
+        for a in range(len(self.schedule)):
+            state = [self.prices[self.period - 1, a]]
+            mask = np.ones(self.num_agents, dtype=bool)
+            mask[a] = False
+            p_ = np.max(self.prices[self.period - 1, mask])
+            p__ = np.max(self.prices[self.period - 2, mask])
+            # state_ = [[self.prices[self.period - 1, a_], self.prices[self.period - 2, a_]] for a_ in np.arange(self.num_agents)[np.r_[0:a, a+1:self.num_agents]]]
+            # state_ = sum(state_, [])
+            state_ = [p_, p__]
+            state.extend(state_)
+            state.append(self.shares[self.period - 1, a])
+            action.append(self.schedule[a].select_action(state)[0])
+        done = False
+
+        self.prices[self.period, :] = action
+
+        self.demand[self.period, :] = demand(action, shares[self.period, :], self.num_agents)
+        self.profits[self.period, :] = profit(action, shares[self.period, :], self.num_agents)
+
+        reward = self.profits[self.period, :]
+
+        for a in range(len(self.schedule)):
+            self.schedule[a].buffer.rewards.append(reward[a])
+            self.schedule[a].buffer.is_terminals.append(done)
+            # update PPO agent
+            if len(self.schedule[a].buffer.rewards) > 10:
+                self.schedule[a].update()
+
+            # if continuous action space; then decay action std of ouput action distribution
+            if has_continuous_action_space and self.period % action_std_decay_freq == 0:
+                self.schedule[a].decay_action_std(action_std_decay_rate, min_action_std)
+
+        self.period += 1
+        if self.period == iterations:
+            for a in range(len(self.schedule)):
+                self.schedule[a].save(checkpoint_path='{}/agent_{}.pth'.format(checkpoint_path, a))
+
+
+def demand(p, share, n_firms):
+    d = [1 - p[firm] * (1 - share[firm]) + 0.5 * np.max(p[:firm] + p[firm+1:]) for firm in range(len(p))]
+    return d
+
+
+def profit(p, share, n_firms):
+    pi = np.array(p) * demand(p, share, n_firms)
+    return pi
+
+
+its = 2 * 10 ** 3
+num_agents = 2
+has_continuous_action_space = True  # continuous action space; else discrete
+shares = np.arange(0.01, 1, 0.01)
+shares = np.hstack([shares, shares[::-1]])
+# shares = np.hstack([shares.reshape(-1, 1), (1 - shares.reshape(-1, 1)) * shares[::-1].reshape(-1, 1), (1 - shares.reshape(-1, 1)) * (1 - shares[::-1].reshape(-1, 1))])
+# shares = np.vstack([shares, shares[:, [2, 0, 1]], shares[:, [1, 2, 0]]])
+shares = np.hstack([shares.reshape(-1, 1), 1 - shares.reshape(-1, 1)])
+episodes = shares.shape[0] * its
+shares = np.tile(shares, (its, 1))
+
+max_ep_len = 1000  # max timesteps in one episode
+max_training_timesteps = int(1000000)  # break training loop if timeteps > max_training_timesteps
+
+
+action_std = 0.6  # starting std for action distribution (Multivariate Normal)
+action_std_decay_rate = 0.05  # linearly decay action_std (action_std = action_std - action_std_decay_rate)
+min_action_std = 0.1  # minimum action_std (stop decay after action_std <= min_action_std)
+action_std_decay_freq = int(3 * 10 ** 4)  # action_std decay frequency (in num timesteps)
+update_timestep = max_ep_len * 4  # update policy every n timesteps
+K_epochs = 80  # update policy for K epochs in one PPO update
+
+eps_clip = 0.2  # clip parameter for PPO
+gamma = 0.99  # discount factor
+
+lr_actor = 0.0003  # learning rate for actor network
+lr_critic = 0.001  # learning rate for critic network
+
+random_seed = 0  # set random seed if required (0 = no random seed)
+checkpoint_path = '{}/{}/models'.format(path_, 'programs' if local else 'pythonFiles')
+state_dim = 4
+
+# action space dimension
+if has_continuous_action_space:
+    action_dim = 1
+else:
+    action_dim = 1
+
+iterations = 1
+
+model = CollusionModel(n=num_agents, iterations=episodes, shares=shares)
+strg_price = np.empty((episodes, num_agents, iterations))
+strg_demand = np.empty((episodes, num_agents, iterations))
+strg_profit = np.empty((episodes, num_agents, iterations))
+
+for it in range(iterations):
+    for episode in tqdm(range(episodes)):
+        model.step()
+    strg_price[:, :, it] = model.prices
+    strg_demand[:, :, it] = model.demand
+    strg_profit[:, :, it] = model.profits
+
+    model.initialize(iterations=episodes, n=num_agents)
+
+
+slice_ = iterations - 1
+labels = ['Firm {}'.format(i) for i in range(num_agents)]
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+ax1.plot(strg_price[:, :, slice_], label=labels)
+ax1.title.set_text('Price')
+ax2.plot(strg_demand[:, :, slice_], label=labels)
+ax2.title.set_text('Demand')
+ax3.plot(strg_profit[:, :, slice_], label=labels)
+ax3.title.set_text('Profit')
+fig.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.4, hspace=0.4)
+fig.legend(labels, loc='upper right', bbox_transform=fig.transFigure)
+
+plt.savefig('{}/figures/SimPPO.png'.format(path_))
